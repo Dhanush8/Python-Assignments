0ec8b81762143ae72be3a06b8a49f8392ec0ac98
Kasun Dhanushka <kasundanushka503@gmail.com>
2021-08-21 10:36:41 +0530

    Update questionNo4.py

--- Assignment 1/questionNo4.py
+++ Assignment 1/questionNo4.py
@@ -1,6 +1,6 @@
-u = str(input('Enter the Unit of Value(cm/m): '))  # u=input unit
+unit = str(input('Enter the Unit of Value(cm/m): '))  # unit=input unit
 x = float(input('Enter the value: '))  # x=input value
-if u == 'cm':
+if unit == 'cm':
     i = 0.393701*x  # i=inches
     f = i/12  # f=feets
     y = 0.0109361*x  # y=yards
@@ -8,7 +8,7 @@ if u == 'cm':
     print('Inches:', i)
     print('Feet:', f)
     print('Yards:', y)
-elif u == 'm':
+elif unit == 'm':
     i = 0.393701*x*100
     f = i/12
     y = 0.0109361*x*100
